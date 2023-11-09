91815a62dab8d3d242266a62af7d883bd988615c
Adeolu Abiodun <138899958+AdeoluAbiodun@users.noreply.github.com>
2023-11-09 16:45:45 -0500

    fixed scripts link issue

--- scripts/fizzbuzz0.js
+++ scripts/fizzbuzz0.js
@@ -1,14 +1,16 @@
-function showAlert() {
+function showAlert(event) {
+    // Prevent the default form submission
+    event.preventDefault();
+
     let firstName = document.getElementById("first-name").value;
     let middleInitial = document.getElementById("middle-initial").value;
     let lastName = document.getElementById("last-name").value;
 
-    /* Check if any of the fields are empty
+    // Check if any of the fields are empty
     if (!firstName || !lastName) {
         alert("Please enter both first and last names.");
         return;
     }
-    */
 
     let message = "Welcome, " + firstName + " " + middleInitial + "." + " " + lastName + "!";
     alert(message);
@@ -16,12 +18,11 @@ function showAlert() {
     // Ask the user how high they want to count
     let count = parseInt(prompt("How high do you want to count, " + firstName + "?"));
 
-    /*// Check if the count is a valid number
+    // Check if the count is a valid number
     if (isNaN(count)) {
         alert("Please enter a valid number for counting.");
         return;
     }
-    */
 
     // Clear previous results
     document.getElementById('fizzbuzz-results').innerHTML = '';
@@ -33,6 +34,7 @@ function showAlert() {
         document.getElementById('fizzbuzz-results').innerHTML += `<p>${i} is ${evenOrOdd}.</p>`;
     }
 }
+
 function clearForm() {
     // Clear the form fields
     document.getElementById("first-name").value = "";
