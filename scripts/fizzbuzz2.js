683954c1caa350c8c04352fe00b6ce634616b7f8
Adeolu Abiodun <138899958+AdeoluAbiodun@users.noreply.github.com>
2023-11-09 06:52:45 -0500

    Create fizzbuzz2.js

---
+++ scripts/fizzbuzz2.js
@@ -0,0 +1,25 @@
+    function isEvenlyDivisible(num1, num2) {
+        return num1 % num2 === 0;
+    }
+
+    function showAlert() {
+        let firstName = document.getElementById("first-name").value;
+        let middleInitial = document.getElementById("middle-initial").value;
+        let lastName = document.getElementById("last-name").value;
+        alert("Welcome, " + firstName + " " + middleInitial + "." + " " + lastName + "!");
+
+        // Clear previous results
+        document.getElementById('fizzbuzz-results').innerHTML = '';
+
+        // Perform the count from 1 to 140 and indicate whether each number is evenly divisible by 3 only
+        for (let i = 1; i <= 140; i++) {
+            const isEvenlyDivisibleBy3 = isEvenlyDivisible(i, 3);
+            const result = isEvenlyDivisibleBy3 ? "Divisible by 3" : "Not Divisible by 3";
+            document.getElementById('fizzbuzz-results').innerHTML += `<p>${i}. ${result}</p>`;
+        }
+    } 
+      function clearForm() {
+        document.getElementById("first-name").value = "";
+        document.getElementById("middle-initial").value = "";
+        document.getElementById("last-name").value = "";
+    }
