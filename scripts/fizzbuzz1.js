5fba011d80fc4f1ad047751aaec488f63688baee
Adeolu Abiodun <138899958+AdeoluAbiodun@users.noreply.github.com>
2023-11-17 09:13:30 -0500

    Update fizzbuzz1.js

--- scripts/fizzbuzz1.js
+++ scripts/fizzbuzz1.js
@@ -1,39 +1,18 @@
 function updateHeader(headerTitle) {
-    // Update the header title based on the current page
     document.querySelector('header h1').textContent = `Adeolu Abiodun's Angelic Angelfish | ${headerTitle}`;
 }
 
 function injectNavLinks() {
-    // Define the common navigation links
-    const navLinksHTML = `
-        <nav>
-            <a href="index.html">Home</a>&nbsp;||
-            <a href="introduction.html">Introduction</a>&nbsp;||
-            <a href="contract.html">Contract</a>&nbsp;||
-            <a href="brand.html">Brand</a>&nbsp;||
-            <a href="fizzbuzz0.html">Fizz Buzz 0</a>&nbsp;||
-            <a href="fizzbuzz1.html">Fizz Buzz 1</a>&nbsp;||
-            <a href="fizzbuzz2.html">Fizz Buzz 2</a>
-        </nav>
-    `;
+    // ... (same as before)
+    document.getElementById('dynamic-nav').innerHTML = navLinksHTML;
 
-    // Inject the navigation links into the header
-    document.querySelector('header').insertAdjacentHTML('beforeend', navLinksHTML);
+    // Attach event listeners after injecting navigation links
+    document.getElementById('fizzBuzz0').addEventListener('click', () => updateHeader("Fizz Buzz 0"));
+    document.getElementById('fizzBuzz1').addEventListener('click', () => updateHeader("Fizz Buzz 1"));
+    document.getElementById('fizzBuzz2').addEventListener('click', () => updateHeader("Fizz Buzz 2"));
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Inject navigation links on page load
-    injectNavLinks();
-
-    // Add event listeners for dynamic header update
-    document.querySelectorAll('nav a').forEach(link => {
-        link.addEventListener('click', (event) => {
-            const pageName = link.textContent;
-            updateHeader(pageName);
-        });
-    });
-});
-
+document.addEventListener('DOMContentLoaded', injectNavLinks);
 
 function showAlert() {
         let firstName = document.getElementById("first-name").value;
@@ -62,7 +41,7 @@ function clearForm() {
     document.getElementById('fizzbuzz-results').innerHTML = "";
 }
 
-//Dynamic syntax
+/* Dynamic syntax
 
 document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('fizzBuzz0').addEventListener('click', () => {
@@ -77,3 +56,4 @@ document.addEventListener('DOMContentLoaded', (event) => {
         updateHeader("Fizz Buzz 2");
     });
 });
+*/
